1b7a172492f777551c5fe33437e5772c086f5452
CodingBazaar <sagarjoshi612@gmail.com>
2021-01-12 18:00:16 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 
 import { HeroFormComponent } from "./hero-form/hero-form.component";
-import { HomeComponent } from "./components/home/home.component";
-import { AddProductComponent } from "./components/add/add.component";
+// import { HomeComponent } from "./components/home/home.component";
+// import { AddProductComponent } from "./components/add/add.component";
+
+import { AppRoutingModule } from "./app.routing.module";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
-    HeroFormComponent,
-    HomeComponent,
-    AddProductComponent
+    HeroFormComponent
+    // HomeComponent,
+    // AddProductComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
